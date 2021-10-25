5d8218d849b0a44ca1c7345b9f491a48c8793079
DanteAleks <carloaleksandr@hotmail.com>
2021-10-25 18:09:09 -0300

    Funcionando
    agora sim

--- app.R
+++ app.R
@@ -14,91 +14,80 @@ ui <- fluidPage(
     
     sidebarPanel(
       
-  dateInput("date1", "Date:",
-            language = "pt",
-            startview = "decade"),
-  dateInput("date2", "Date:",
-            language = "pt",
-            startview = "decade"),
-  numericInput("Valoranterior", label = "Cotacao dia anterior:", NA, min = -10000, max = 10000),
-  
-  
-  numericInput("margem", label = "Margem desejada:", NA, min = -10000, max = 10000),
-  
-  numericInput("Valordia", label = "Valor Atual:", NA, min = -10000, max = 10000),
-  
-  actionButton("do", " $$$ ")
+      dateInput("date1", "Date:",
+                language = "pt",
+                startview = "year",
+                daysofweekdisabled = c(0,6),
+                value = Sys.Date()-10),
+      dateInput("date2", "Date:",
+                language = "pt",
+                value = Sys.Date(),
+                daysofweekdisabled = c(0,6)),
+      
+      selectInput("nomeacao", "Acao:",
+                  c("PETR3.SA",
+                    "VALE3.SA",
+                    "SANB11",
+                    "TRPL4",
+                    "HYPE3",
+                    "SBSP3",
+                    "EMBR3",
+                    "PGMN3",
+                    "ITUB4",
+                    "JBSS3",
+                    "RDOR3",
+                    "^BVSP")),
+      
+      
+      
+      actionButton("do", " $$$ ")
     ),
-  
-  mainPanel(
-    verbatimTextOutput("summary1")
-  )
+    
+    mainPanel(
+      
+      textOutput("result1"),
+      textOutput("result2"),
+      textOutput("result3"),
+      plotOutput("plot1")
+      
+    )
   )
 )
 server <- function(input, output) {
   
   reactive({
-    Inicio<-as.date(input$date1)
-    Fim<-input$date2
-    Valoranterior<-input$Valoranterior
-    margem<-input$margem
-    VALORDIA<-input$Valordia
+    input$date1
+    input$date2
+    input$nomeacao
+  })
+  
+  
+  
+  
+  
+  observeEvent(input$do,{
+    
+    output$result1 <- renderText({
+      paste("Data inicial escolhida:", input$date1)
     })
-  
- 
-  
-  # output$data1 <- renderText({ input$date1 })
-  # output$data2 <- renderText({ input$date2 })
-  # output$Valoranterior <- renderText({ input$Valoranterior })
-  # output$margem <- renderText({ input$margem })
-  # output$VALORDIA <- renderText({ input$Valordia })
-  
-  
-  
-  observeEvent(input$do, {
+    output$result2 <- renderText({
+      paste("Data final escolhida:", input$date2)
+    }) 
+    output$result3 <- renderText({
+      paste("Açao escolhida:", input$nomeacao)
+    })    
     
-  bov <- getSymbols("^BVSP", src = "yahoo", 
-                    from = Inicio, to = Fim, auto.assign = FALSE)
     
-  
-  
-  bov1 <- as.data.frame(bov) %>% mutate(Data = index(bov)) %>% na.omit()
-  
-  bov1<- bov1  %>% mutate(Open = as.numeric(BVSP.Open), High = as.numeric(BVSP.High), 
-                          Low = as.numeric(BVSP.Low), Close = as.numeric(BVSP.Close), Adj.Close = as.numeric(BVSP.Adjusted),
-                          Volume = as.numeric(BVSP.Volume)) %>% 
-    select(Data, Open, High, Low, Close, Adj.Close) %>% 
-    mutate(GP = Open - Close) %>% mutate( DGP = ifelse(GP< 0, "PERDA","GANHO"))
-  bov3 <-bov1 %>% mutate(NextDAYP=0, NextGPP=0,NextDAYG=0, NextGPG=0, NEXTGP=0)
-  
-  
-  for(i in 1:dim(bov3)[1]) {
-    if(bov3$GP[i]< 0){
-      bov3$NextDAYP[i]=bov3$DGP[i+1]
-      bov3$NextGPP[i]=bov3$GP[i+ 1]
-      bov3$NEXTGP[i]=bov3$GP[i+ 1]
-    } 
+    bov <- getSymbols(input$nomeacao, src = "yahoo",
+                      from = input$date1, to = input$date2, auto.assign = FALSE)
     
-  }
-  for(i in 1:dim(bov3)[1]) {
-    if(bov3$GP[i]> 0){
-      bov3$NextDAYG[i]=bov3$DGP[i+1]
-      bov3$NextGPG[i]=bov3$GP[ i+ 1] 
-      bov3$NEXTGP[i]=bov3$GP[i+ 1]
-    }
+    output$plot1<-renderPlot({
+      ggplot(bov, aes(x = index(bov), y = bov[,6])) + geom_line(color = "darkblue") +
+        ggtitle(paste("Série de preços"), input$nomeacao) +
+        xlab("Data") + ylab("Preço ($)") + theme(plot.title = element_text(hjust = 0.5)) + 
+        scale_x_date(date_labels = "%b %y", date_breaks = "6 months")
+    }) 
     
-  }
-  
-  bov3gp3<- bov3 %>% mutate(NextDAYP = ifelse(NextDAYP == 0 & NextGPP == 0, NA, NextDAYP), 
-                            NextGPP = ifelse(NextDAYP == 0 & NextGPP == 0, NA, NextGPP)) %>% 
-    mutate(NextDAYG = ifelse(NextDAYG == 0 & NextGPG == 0, NA, NextDAYG), 
-           NextGPG = ifelse(NextDAYG == 0 & NextGPG == 0, NA, NextGPG)) %>% 
-    mutate(DGP = as.factor(DGP), NextDAYP = as.factor(NextDAYP), NextDAYG = as.factor(NextDAYG)) %>% 
-    mutate( NEXTDGP = ifelse(NEXTGP< 0, "PERDA","GANHO"))
-  
-  
-  
-  output$summary1 <- renderPrint(summary(bov3gp3))
   })
 }
 
